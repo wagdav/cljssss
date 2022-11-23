efa6577dae283fce7aba31d1e6822a6e51e25abf
David Wagner <wagdav@gmail.com>
2022-11-23 17:05:38 +0100

    Add battlesnake CLI

--- flake.nix
+++ flake.nix
@@ -3,9 +3,27 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.battlesnake-cli =
+      with import nixpkgs { system = "x86_64-linux"; };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      buildGoModule rec {
+        pname = "battlesnake-cli";
+        version = "2.0.0";
 
+        src = fetchFromGitHub {
+          owner = "BattleSnakeOfficial";
+          repo = "rules";
+          rev = "v${version}";
+          sha256 = "sha256-Ly8/g3Q+ybKGOvyORoYm3YeVrHdgtGij0uFFYh9ex9w=";
+        };
+
+        vendorSha256 = "sha256-tGOxBhyOPwzguRZY4O2rLoOMaT3EyryjYcO2/2GnVIU=";
+        subPackages = [ "cli/battlesnake" ];
+      };
+
+    apps.x86_64-linux.battlesnake = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.battlesnake-cli}/bin/battlesnake";
+    };
   };
 }
