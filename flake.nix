8c89ed20205bb7f438148318aee70f9c7dafbb0c
David Wagner <wagdav@gmail.com>
2022-11-23 17:27:31 +0100

    Make muti-platform and use stable channel

--- flake.nix
+++ flake.nix
@@ -1,29 +1,36 @@
 {
-  description = "A very basic flake";
+  description = "A Clojure Battlesnake";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.battlesnake-cli =
-      with import nixpkgs { system = "x86_64-linux"; };
-
-      buildGoModule rec {
-        pname = "battlesnake-cli";
-        version = "2.0.0";
-
-        src = fetchFromGitHub {
-          owner = "BattleSnakeOfficial";
-          repo = "rules";
-          rev = "v${version}";
-          sha256 = "sha256-Ly8/g3Q+ybKGOvyORoYm3YeVrHdgtGij0uFFYh9ex9w=";
-        };
-
-        vendorSha256 = "sha256-tGOxBhyOPwzguRZY4O2rLoOMaT3EyryjYcO2/2GnVIU=";
-        subPackages = [ "cli/battlesnake" ];
-      };
-
-    apps.x86_64-linux.battlesnake = {
-      type = "app";
-      program = "${self.packages.x86_64-linux.battlesnake-cli}/bin/battlesnake";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.battlesnake-cli =
+
+          pkgs.buildGoModule rec {
+            pname = "battlesnake-cli";
+            version = "2.0.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "BattleSnakeOfficial";
+              repo = "rules";
+              rev = "v${version}";
+              sha256 = "sha256-Ly8/g3Q+ybKGOvyORoYm3YeVrHdgtGij0uFFYh9ex9w=";
+            };
+
+            vendorSha256 = "sha256-tGOxBhyOPwzguRZY4O2rLoOMaT3EyryjYcO2/2GnVIU=";
+            subPackages = [ "cli/battlesnake" ];
+          };
+
+        apps.battlesnake = {
+          type = "app";
+          program = "${self.packages.x86_64-linux.battlesnake-cli}/bin/battlesnake";
+        };
+      });
 }
